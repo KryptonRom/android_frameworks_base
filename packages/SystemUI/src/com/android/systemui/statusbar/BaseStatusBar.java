fbe68554ce2efc5b9b586e93735e4cefb6ef385c
Schubi <schubi@erlangen.ccc.de>
2013-06-15 16:02:41 +0200

    Halo: Add master switch to disable Halo [1/2]
    Change-Id: Icfd9351c2a8a8c4107e2398669d2501dbe5e189a
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -155,9 +155,11 @@ public abstract class BaseStatusBar extends SystemUI implements
     // Halo
     protected Halo mHalo = null;
     protected Ticker mTicker;
+    protected boolean mHaloEnabled;
     protected boolean mHaloActive;
     public boolean mHaloTaskerActive = false;
     protected ImageView mHaloButton;
+    protected boolean mHaloButtonVisible = true;
 
     // UI-specific methods
 
@@ -326,6 +328,9 @@ public abstract class BaseStatusBar extends SystemUI implements
             // If the system process isn't there we're doomed anyway.
         }
 
+        mHaloEnabled = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HALO_ENABLED, 0) == 1;
+
         mHaloActive = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_ACTIVE, 0) == 1;
 
@@ -388,6 +393,14 @@ public abstract class BaseStatusBar extends SystemUI implements
                 updateHalo();
             }});
 
+        // Listen for HALO enabled switch
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.HALO_ENABLED), false, new ContentObserver(new Handler()) {
+            @Override
+            public void onChange(boolean selfChange) {
+                updateHalo();
+            }});
+
         mContext.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.HALO_SIZE), false, new ContentObserver(new Handler()) {
             @Override
@@ -405,6 +418,17 @@ public abstract class BaseStatusBar extends SystemUI implements
         }
     }
 
+    protected void updateHaloButton() {
+        if (mHaloEnabled) {
+            mHaloButtonVisible = true;
+        } else {
+            mHaloButtonVisible = false;
+        }
+        if (mHaloButton != null) {
+            mHaloButton.setVisibility(mHaloButtonVisible && !mHaloActive ? View.VISIBLE : View.GONE);
+        }
+    }
+
     public void restartHalo() {
         if (mHalo != null) {
             mHalo.cleanUp();
@@ -416,12 +440,19 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     protected void updateHalo() {
+        mHaloEnabled = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HALO_ENABLED, 0) == 1;
+
         mHaloActive = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_ACTIVE, 0) == 1;
 
         mHaloButton.setImageResource(mHaloActive
                 ? R.drawable.ic_notify_halo_pressed
                 : R.drawable.ic_notify_halo_normal);
+
+        if (!mHaloEnabled) {
+          mHaloActive = false;
+        }
 
         if (mHaloActive) {
             if (mHalo == null) {
