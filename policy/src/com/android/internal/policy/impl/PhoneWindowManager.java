207ed78462cc1627254e53a08b4137a5b6b44281
xplodwild <xplod@ouverta.fr>
2013-09-03 20:55:56 +0200

    [WIP] Multi-window
    [[[ USE PS 18 IF YOU WANT TO TEST - WORK IN PROGRESS ]]]
    
    This is a big work in progress, which currently has some bugs,
    but begins to look okay.
    
    You can already multitask a little bit, by using the
    app switcher and long-pressing an icon, then select "Add to
    Split View".
    Last window touched gets the key focus.
    
    Code is very ugly, I know. Don't merge it, don't use it
    for now. Unless you want to make your code ugly.
    
    Enjoy :)
    
    Change-Id: I4a03a84539fd3ac1273f0d3503fe8fafcd16be26
    
    Conflicts:
    
    	core/java/android/app/IActivityManager.java
    	packages/SystemUI/res/values/custom_strings.xml
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
    
    Conflicts:
    	core/java/android/app/Activity.java
    	core/java/android/content/Intent.java
    	packages/SystemUI/res/values/pa_strings.xml
    	packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -3141,6 +3141,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
+    public Rect getContentRect() {
+        return new Rect(mContentLeft, mContentTop, mContentRight, mContentBottom);
+    }
+
     /** {@inheritDoc} */
     @Override
     public void layoutWindowLw(WindowState win, WindowManager.LayoutParams attrs,
