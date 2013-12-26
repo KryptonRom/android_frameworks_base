e8cb983beb7427a44c2845e0f6439797142348aa
0xD34D <clark@scheffsblend.com>
2013-12-26 03:42:47 -0600

    Implement new-ER ChaOS sidebar / Add ChaOS labs Only a little error, if you choose right side, you'll get FC Conflicts: 	core/java/android/provider/Settings.java 	packages/SystemUI/AndroidManifest.xml 	packages/SystemUI/res/values/colors.xml 	packages/SystemUI/res/values/custom_strings.xml 	packages/SystemUI/res/values/custom_styles.xml 	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java 	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3008,6 +3008,62 @@ public final class Settings {
         public static final String QUIET_HOURS_STILL = "quiet_hours_still";
 
         /**
+         * Whether or not to use the app sidebar
+         *
+         * @hide
+         */
+        public static final String APP_SIDEBAR_ENABLED = "app_sidebar_enabled";
+
+        /**
+         * User defined transparency level for sidebar
+         *
+         * @hide
+         */
+        public static final String APP_SIDEBAR_TRANSPARENCY = "app_sidebar_transparency";
+
+        /**
+         * Disable text labels for app sidebar items
+         *
+         * @hide
+         */
+        public static final String APP_SIDEBAR_DISABLE_LABELS = "app_sidebar_disable_labels";
+
+        /**
+         * Position of app sidebar
+         *
+         * @hide
+         */
+        public static final String APP_SIDEBAR_POSITION = "app_sidebar_position";
+
+        /**
+         * Width of the appbar trigger
+         *
+         * @hide
+         */
+        public static final String APP_SIDEBAR_TRIGGER_WIDTH = "app_sidebar_trigger_width";
+
+        /**
+         * Position of appbar trigger
+         *
+         * @hide
+         */
+        public static final String APP_SIDEBAR_TRIGGER_TOP = "app_sidebar_trigger_top";
+
+        /**
+         * Height of the appbar trigger
+         *
+         * @hide
+         */
+        public static final String APP_SIDEBAR_TRIGGER_HEIGHT = "app_sidebar_trigger_height";
+ 
+          /**
+         * Whether to display the trigger region or not
+         *
+         * @hide
+         */
+        public static final String APP_SIDEBAR_SHOW_TRIGGER = "app_sidebar_show_trigger";
+
+        /**
          * Whether to attempt to dim the LED color during quiet hours.
          * @hide
          */
@@ -3210,13 +3266,6 @@ public final class Settings {
          * @hide
          */
         public static final String APP_SIDEBAR_SORT_TYPE = "app_sidebar_sort_type";
-
-        /**
-         * How to sort and order the items in the app sidebar
-         *
-         * @hide
-         */
-        public static final String APP_SIDEBAR_TRANSPARENCY = "app_sidebar_transparency";
 
         /**
          * Width and height of items in sidebar
@@ -7415,4 +7464,3 @@ public final class Settings {
         return "android-" + Long.toHexString(androidId);
     }
 }
-
